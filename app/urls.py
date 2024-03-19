47baf5fe27d5511e03bd4d74b1a7c248d4c016c2
caiomaz <caiotmzferreira@gmail.com>
2024-03-19 19:34:22 -0300

    adicionar lógica de login e logout

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('index/', IndexView.as_view(), name='index'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Produto
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'erro': 'Usuário ou senha inválidos'})
 
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
+
+@method_decorator(login_required, name='dispatch')
 class IndexView(View):
     def get(self, request):
         produtos = Produto.objects.all()
         return render(request, 'index.html', {'produtos': produtos})
     def post(self, request):
-        pass
+        return render(request, 'index.html')
